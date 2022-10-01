efa8660a52174d98156a9926a1de50f923ffa7fd
Samuel Njenga <snjenga704@gmail.com>
2022-10-01 22:07:36 +0300

    feat: add useUpdateLogger.js which logs value of the changing state name

--- src/hooks/set_one/LocalStorage.js
+++ src/hooks/set_one/LocalStorage.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 import useLocalStorage from "./useLocalStorage";
+import useUpdateLogger from "./useUpdateLogger";
 
 const LocalStorage = () => {
   const [name, setName] = useLocalStorage("name", "");
+  useUpdateLogger(name);
+
   return (
     <div>
       <input value={name} onChange={(e) => setName(e.target.value)} />
