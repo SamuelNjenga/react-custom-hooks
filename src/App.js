8ea0174ffd4cef40e4d04b82c584e04f72877155
Samuel Njenga <snjenga704@gmail.com>
2022-10-02 20:02:06 +0300

    feat: add useDebounce.js hook which results in a functionality execution after a delay based on a dependency

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LocalStorage from "./hooks/set_one/LocalStorage";
+import DebounceComponent from "./hooks/set_two/DebounceComponent";
 import TimeoutComponent from "./hooks/set_two/TimeoutComponent";
 import ToggleComponent from "./hooks/set_two/ToggleComponent";
 
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/localStorage" element={<LocalStorage />} />
           <Route path="/toggleValue" element={<ToggleComponent />} />
           <Route path="/timeout" element={<TimeoutComponent />} />
+          <Route path="/debounce" element={<DebounceComponent />} />
         </Routes>
       </Router>
     </div>
---
+++ src/hooks/set_two/DebounceComponent.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+import useDebounce from "./useDebounce";
+
+const DebounceComponent = () => {
+  const [count, setCount] = useState(10);
+  useDebounce(() => alert(count), 1000, [count]);
+  
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default DebounceComponent;
