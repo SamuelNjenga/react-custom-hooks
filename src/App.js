b6c4e384b81e8199c0a345c27824b7fa0b942449
Samuel Njenga <snjenga704@gmail.com>
2022-10-03 08:46:47 +0300

    feat: add useArray.js which returns a series of array functions to be used in other components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LocalStorage from "./hooks/set_one/LocalStorage";
+import ArrayComponent from "./hooks/set_two/ArrayComponent";
 import DebounceComponent from "./hooks/set_two/DebounceComponent";
 import TimeoutComponent from "./hooks/set_two/TimeoutComponent";
 import ToggleComponent from "./hooks/set_two/ToggleComponent";
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/timeout" element={<TimeoutComponent />} />
           <Route path="/debounce" element={<DebounceComponent />} />
           <Route path="/updateEffect" element={<UpdateEffectComponent />} />
+          <Route path="/arrays" element={<ArrayComponent />} />
         </Routes>
       </Router>
     </div>
