c226d104a21e3deef8cd156239e57d75eafb3df3
Samuel Njenga <snjenga704@gmail.com>
2022-10-03 12:56:55 +0300

    feat: add useFetch hook which allows the sending of an api request and return of a response

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LocalStorage from "./hooks/set_one/LocalStorage";
 import AsyncComponent from "./hooks/set_three/AsyncComponent";
+import FetchComponent from "./hooks/set_three/FetchComponent";
 import PreviousComponent from "./hooks/set_three/PreviousComponent";
 import StateWithHistoryComponent from "./hooks/set_three/StateWithHistoryComponent";
 import StorageComponent from "./hooks/set_three/StorageComponent";
@@ -28,6 +29,7 @@ const App = () => {
           <Route path="/historyState" element={<StateWithHistoryComponent />} />
           <Route path="/stateStorage" element={<StorageComponent />} />
           <Route path="/async" element={<AsyncComponent />} />
+          <Route path="/fetch" element={<FetchComponent />} />
         </Routes>
       </Router>
     </div>
